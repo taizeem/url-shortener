86d882e509dcfe444d14eb870e11f3141433f69c
taizeem0@gmail.com <taizeem0@gmail.com>
2026-08-09 09:23:14 +0530

    add url creation endpoin and connected that to database

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,5 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.schemas.url import URLCreate
+from sqlalchemy.orm import Session
+
+from app.core.database import get_db
+from app.models.urls import URL
+from app.schemas.url import URLCreate
+from app.utils.short_code import generate_short_code
 
 
 router = APIRouter(
@@ -9,7 +15,20 @@ router = APIRouter(
 
 @router.post("/")
 
-def create_short_url(data: URLCreate):
+def create_short_url(data: URLCreate, db:Session = Depends(get_db)):
+    short_code = generate_short_code()
+    url = URL(
+        original_url = str(data.original_url),
+        short_code = short_code,
+    )
+
+    db.add(url)
+    db.commit()
+    db.refresh(url)
+
     return {
-        "orignial_url": str(data.original_url)
+        "id": url.id,
+        "original_url": url.original_url,
+        "short_code": url.short_code
     }
+    
